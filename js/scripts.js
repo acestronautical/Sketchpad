69c4b0537cd8216b95759913364b1ac320c9ba8e
Ace Cassidy <ac27182@gmail.com>
2016-05-18 14:22:57 -0700

    improved box generationg, still buggg though

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
-boxSize = 12;
+var boxSize = 12;
 var numBoxes = (460800 / (boxSize * boxSize));
-var container = $('#container');
-var newBox = "<div class='box' onmouseover='this.style.backgroundColor = \"rgb(46, 46, 46)\";' id='[id]' ></div>";
+var color = 'rgb(46, 46, 46)'
+var newBox = "<div class='box' onmouseover='this.style.backgroundColor = \"" + color + "\";' id='[id]' ></div>";
 
 function boxGen(boxSize) {
-container.empty();
+$('#container').empty();
 
-for(i = 1; i <= (460800 / (boxSize * boxSize)); i++){
-  container.append(newBox.replace('[id]', i));
-}
-// $(Array(460800 / (boxSize * boxSize)).join(newBox));
+$('#container').append($(Array(460800 / (boxSize * boxSize)).join(newBox)));
+
 };
 
 $(document).ready(function(){
+
   boxGen(boxSize);
+
   $('.box').css({"height": boxSize + "px", "width" : boxSize + "px"});
 
   $('#shake').click(function(){
@@ -22,13 +22,7 @@ $(document).ready(function(){
   });
 
   $('#gridSize').click(function(){
-    if(boxSize <= 30){
-      boxSize += 6;
-    } else{
-      boxSize = 12;
-    };
     boxGen(boxSize);
-    $( "#border" ).effect( "shake" );
   });
 
 });
