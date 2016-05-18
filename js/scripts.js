63306c9f14e30efa376afe3075034a1dd8437965
Ace Cassidy <ac27182@gmail.com>
2016-05-18 11:00:35 -0700

    Added shake functionality to etch-a-sketch

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,16 @@ $(document).ready(function(){
   var container = $('#container');
   var newBox = "<div class='box' onmouseover='this.style.backgroundColor = \"rgb(46, 46, 46)\";' id='[id]' ></div>";
 
+  $('#shake').click(function(){
+
+    $( "#border" ).effect( "shake" );
+
+    for(x = 1; x <= numBoxes; x++){
+    $('#' + x).css('background-color', 'white');
+    };
+
+});
+
   for(i = 1; i <= numBoxes; i++){
     container.append(newBox.replace('[id]', i));
   };
