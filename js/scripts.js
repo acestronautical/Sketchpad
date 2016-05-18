a66886a26c7204acc83408954002229351aff7e2
Ace Cassidy <ac27182@gmail.com>
2016-05-18 14:59:18 -0700

    Grid size changing fully functional

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@ var numBoxes = (460800 / (boxSize * boxSize));
 var color = 'rgb(46, 46, 46)'
 var newBox = "<div class='box' onmouseover='this.style.backgroundColor = \"" + color + "\";' id='[id]' ></div>";
 
-function boxGen() {
+function boxGen(boxSize) {
 $('#container').empty();
-
 $('#container').append($(Array(460800 / (boxSize * boxSize)).join(newBox)));
+$('.box').css({"height": boxSize + "px", "width" : boxSize + "px"});
 
 };
 
@@ -14,15 +14,18 @@ $(document).ready(function(){
 
   boxGen(boxSize);
 
-  $('.box').css({"height": boxSize + "px", "width" : boxSize + "px"});
-
   $('#shake').click(function(){
     $( "#border" ).effect( "shake" );
     $('.box').css('background-color', 'white');
   });
 
   $('#gridSize').click(function(){
-    boxGen();
+    if(boxSize <= 20){
+      boxSize += 4;
+    } else{
+      boxSize = 8;
+    }
+    boxGen(boxSize);
   });
 
 });
