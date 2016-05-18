135e1c0ef06c687f4af3d9bd02f35f210c9a4852
Ace Cassidy <ac27182@gmail.com>
2016-05-17 17:47:53 -0700

    Core functionality completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
 
-  var box = $('div').css();
+  var boxSize = prompt('size of boxes?');
+  var numBoxes = (460800 / (boxSize * boxSize));
+  var container = $('#container');
+  var newBox = "<div class='box' onmouseover='this.style.backgroundColor = \"black\";' id='[id]' ></div>";
+
+  for(i = 1; i <= numBoxes; i++){
+    container.append(newBox.replace('[id]', i));
+  };
+
+  $('.box').css({"height": boxSize + "px", "width" : boxSize + "px"});
 
 
-
+  // $('.box').mouseenter(function(){
+  //   $(this).css('background-color','black');
+  // });
 
 });
